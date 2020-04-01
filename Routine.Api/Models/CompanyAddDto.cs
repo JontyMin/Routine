2b0dfb5dfff307f25586c72e67c9972f9dfcc897
JontyWang <501211312@qq.com>
2020-04-01 21:14:21 +0800

    同时创建

--- Routine.Api/Models/CompanyAddDto.cs
+++ Routine.Api/Models/CompanyAddDto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 
 namespace Routine.Api.Models
@@ -9,5 +10,7 @@ namespace Routine.Api.Models
     {
         public string Name { get; set; }
         public string Introduction { get; set; }
+
+        public ICollection<EmployeeAddDto> Employees { get; set; }=new List<EmployeeAddDto>();
     }
 }
